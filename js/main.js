bce900ec6e0e011ee48f530357bea43d8fb206f1
smontoro <sarahjmontoro@gmail.com>
2017-11-19 12:37:31 -0800

    added a loop to append gender

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 
 function submitForm() {
 
-	var main = document.getElementById("main")
 	var first = document.getElementById("first").value
 	var last = document.getElementById("last").value
-	var pWelcome = document.createElement("p")
-		pWelcome.className = "welcome"
-		pWelcome.innerHTML = "Welcome, " + first + " " + last
+	var newUserName = document.getElementById("newUserName")
+		newUserName.innerHTML = "Welcome, " + first + " " + last
 
-	document.getElementById("main").innerHTML = ""
+	var gender = document.getElementsByClassName("user-gender")
 
-	main.appendChild(pWelcome)
+	var userGender
+
+	//loop through all elements with user-gender class
+	for(i = 0; i < gender.length; i++) {
+		if(gender[i].checked) {
+			userGender = gender[i].value
+		}
+	}
+
+	var newUserGender = document.getElementById("newUserGender")
+		newUserGender.innerHTML = "Gender: " + userGender
+
+// hide new profile form
+	document.getElementById('main').style.display = "none";
+	// display updated profile data
+	document.getElementById('newUser').style.display = "block";
+
+	
 
 
 }
