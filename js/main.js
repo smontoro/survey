3b18458f88798c2d84c55a19a7aaf31976fb4579
smontoro <sarahjmontoro@gmail.com>
2017-11-19 13:15:17 -0800

    added phone value to submit function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 
 function submitForm() {
 
+	/*
+	===NAME===
+	*/
 	var first = document.getElementById("first").value
 	var last = document.getElementById("last").value
 	var newUserName = document.getElementById("newUserName")
 		newUserName.innerHTML = "Welcome, " + first + " " + last
 
+	/*	
+	===GENDER===
+	*/
 	var gender = document.getElementsByClassName("user-gender")
-
 	var userGender
-
 	//loop through all elements with user-gender class
 	for(i = 0; i < gender.length; i++) {
 		if(gender[i].checked) {
@@ -20,12 +24,31 @@ function submitForm() {
 	var newUserGender = document.getElementById("newUserGender")
 		newUserGender.innerHTML = "Gender: " + userGender
 
+	/*
+	===EMAIL===
+	*/
+	var email = document.getElementById("email").value
+	var newUserEmail = document.getElementById("newUserEmail")	
+		newUserEmail.innerHTML = "Email: "+ email
+
+	/*
+	===PHONE===
+	*/
+	var phone = document.getElementById("phone").value
+	var newUserPhone = document.getElementById("newUserPhone")
+		newUserPhone.innerHTML = "Phone: " + phone
+
+	/*
+	===COLOR===
+	*/
+	var color
+
+
 // hide new profile form
 	document.getElementById('main').style.display = "none";
 	// display updated profile data
 	document.getElementById('newUser').style.display = "block";
-
 	
 
 
-}
+};
